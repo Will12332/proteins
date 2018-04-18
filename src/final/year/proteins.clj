a0ab9009d51cc3533a9a638b20936b66bde9a6db
will12332 <williamholder95@gmail.com>
2018-04-18 22:09:50 +0100

    Added some properties, macros don't work - runtime error (no such var)

--- src/final/year/proteins.clj
+++ src/final/year/proteins.clj
@@ -1,48 +1,17 @@
 (ns final.year.proteins
-  (:use [tawny.owl]))
+  (:require 
+     [clojure.string]
+     [tawny.owl :refer :all]
+     [tawny.pattern :as p]
+     [tawny.reasoner :as r]
+     [tawny.util :as u]))     
+            
 
 
 (defontology final-year-proteins
   :iri "http://example.com/final-year-proteins")
 
 (defclass Protein)
-
-
-(as-subclasses
-  Protein
-  :disjoint
-  (defclass CENP-A
-    :label "Centromere-specific histone H3 CENP-A"
-    :label "https://www.pombase.org/gene/SPBC1105.17")
-  (defclass NDC-80
-    :label "NMS complex subunit NDC80"
-    :label "https://www.pombase.org/gene/SPBC11C11.03")
-  (defclass NUF-2
-    :label "NMS complex subunit NUF2"
-    :label "https://www.pombase.org/gene/SPAC27F1.04c")
-  (defclass MIS-12
-    :label "NMS complex subunit MIS12"
-    :label "https://www.pombase.org/gene/SPBC409.04c")
-  (defclass MIS-14
-    :label "NMS complex subunit MIS14"
-    :label "https://www.pombase.org/gene/SPAC688.02c")
-  (defclass MIS-18
-    :label "Kinetochore protein MIS18"
-    :label "https://www.pombase.org/gene/SPCC970.12")
-  (defclass SPC-24
-    :label "NMS complex subunit SPC24"
-    :label "https://www.pombase.org/gene/SPBC336.08")
-  (defclass SPC-25
-    :label "NMS complex subunit SPC25"
-    :label "https://www.pombase.org/gene/SPCC188.04c")
-  (defclass SOS-7
-    :label "NMS complex subunit SOS7"
-    :label "https://www.pombase.org/gene/SPAPB17E12.06")
-  (defclass SPC-7
-    :label "NMS complex subunit SPC7"
-    :label "https://www.pombase.org/gene/SPCC1020.02")
-  (defclass NNF-1)
-  (defclass NSK-1))
 
 (as-subclasses
   Protein
@@ -90,4 +59,50 @@
   :super
   (owl-some binds NDC-80))
 
+
+(defclass TestRole)
+
+(defoproperty hasRole
+  :domain Protein
+  :range TestRole)
+
+(owl-class Protein
+  :super (owl-some hasRole TestRole))
+
+(defclass ProteinProperty)
+
+(defmacro defroles
+  [name values]
+  `(p/deftier ~name ~values
+    :suffix :Role
+    :function false
+    :cover false
+    :domain Protein
+    :superproperty hasRole))
+
+
+(defroles TestRole
+  [ProteinBinding DNABinding])
+
+(p/deftier DNABinding
+  [DoubleStranded Centromeric]
+  :function false
+  :cover false
+  :domain Protein
+  :suffix true
+  :superproperty hasRole)
+
+(p/defpartition CellCycleStage
+  [G1 S G2 Mitotic]
+  :comment "What stage of the Cell Cycle they bind"
+  :super ProteinProperty
+  :domain Protein)
+
+(defmacro defaaproperty [& body]
+  `(p/defpartition
+    ~@body :super ProteinProperty
+    :domain Protein))
+
+(defaaproperty Essentialness
+  [Essential NonEssential])
 
